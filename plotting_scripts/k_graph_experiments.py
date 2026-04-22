a060bbe840c5bc308c17eec0fa4a24fcc78e39c6
tilly <till.aust@uni-konstanz.de>
2026-04-22 09:15:09 +0900

    generate heatmaps

--- plotting_scripts/k_graph_experiments.py
+++ plotting_scripts/k_graph_experiments.py
@@ -28,43 +28,72 @@ heatmap_path_01 = (f"/home/till/PycharmProjects/firefly_two-phase_performance/re
 heatmap_path_02 = (f"/home/till/PycharmProjects/firefly_two-phase_performance/results/"
                 f"compressed_results_k_graph_experiment{experiment_tag}_N={np.min(Ns)}_{np.max(Ns)}_C={np.min(Cs)}_{np.max(Cs)}_T={T}_flash_proportion=0.5_qr_threshold=0.5_update_noise={0.2}.npz")
 
+heatmap_path_00_async = (f"/home/till/PycharmProjects/firefly_two-phase_performance/results/"
+                f"compressed_async_runs_results_k_graph_experiment{experiment_tag}_N={np.min(Ns)}_{np.max(Ns)}_C={np.min(Cs)}_{np.max(Cs)}_T={T}_flash_proportion=0.5_qr_threshold=0.5_update_noise={0.0}.npz")
+heatmap_path_005_async = (f"/home/till/PycharmProjects/firefly_two-phase_performance/results/"
+                f"compressed_async_runs_results_k_graph_experiment{experiment_tag}_N={np.min(Ns)}_{np.max(Ns)}_C={np.min(Cs)}_{np.max(Cs)}_T={T}_flash_proportion=0.5_qr_threshold=0.5_update_noise={0.05}.npz")
+heatmap_path_01_async = (f"/home/till/PycharmProjects/firefly_two-phase_performance/results/"
+                f"compressed_async_runs_results_k_graph_experiment{experiment_tag}_N={np.min(Ns)}_{np.max(Ns)}_C={np.min(Cs)}_{np.max(Cs)}_T={T}_flash_proportion=0.5_qr_threshold=0.5_update_noise={0.1}.npz")
+heatmap_path_02_async = (f"/home/till/PycharmProjects/firefly_two-phase_performance/results/"
+                f"compressed_async_runs_results_k_graph_experiment{experiment_tag}_N={np.min(Ns)}_{np.max(Ns)}_C={np.min(Cs)}_{np.max(Cs)}_T={T}_flash_proportion=0.5_qr_threshold=0.5_update_noise={0.2}.npz")
+
 if os.path.isfile(heatmap_path_00):
     print(f"{heatmap_path_00} already exists. loading...")
     heatmap_00 = np.load(heatmap_path_00)["arr"]
     heatmap_005 = np.load(heatmap_path_005)["arr"]
     heatmap_01 = np.load(heatmap_path_01)["arr"]
     heatmap_02 = np.load(heatmap_path_02)["arr"]
-    
+    heatmap_00_async = np.load(heatmap_path_00_async)["arr"]
+    heatmap_005_async = np.load(heatmap_path_005_async)["arr"]
+    heatmap_01_async = np.load(heatmap_path_01_async)["arr"]
+    heatmap_02_async = np.load(heatmap_path_02_async)["arr"]
+
 else:
     save_flash_counts_00 = {}
     save_flash_counts_005 = {}
     save_flash_counts_01 = {}
     save_flash_counts_02 = {}
+    save_flash_counts_00_async = {}
+    save_flash_counts_005_async = {}
+    save_flash_counts_01_async = {}
+    save_flash_counts_02_async = {}
     for N in Ns:
         save_flash_counts_00[N] = {}
         save_flash_counts_005[N] = {}
         save_flash_counts_01[N] = {}
         save_flash_counts_02[N] = {}
+        save_flash_counts_00_async[N] = {}
+        save_flash_counts_005_async[N] = {}
+        save_flash_counts_01_async[N] = {}
+        save_flash_counts_02_async[N] = {}
         for C in Cs:
             save_flash_counts_00[N][C] = 0.0
             save_flash_counts_005[N][C] = 0.0
             save_flash_counts_01[N][C] = 0.0
             save_flash_counts_02[N][C] = 0.0
+            save_flash_counts_00_async[N][C] = 0.0
+            save_flash_counts_005_async[N][C] = 0.0
+            save_flash_counts_01_async[N][C] = 0.0
+            save_flash_counts_02_async[N][C] = 0.0
+            
             try:
                 data = pd.read_pickle('~/PycharmProjects/firefly_two-phase_performance/results/'
                                    f'k_regular_graph{experiment_tag}/'
                                    f'flash_proportion=0.5_qr_threshold=0.5_update_noise={0.0}/'
                                    f'N={N}_C={C}_T=10000_k_regular_graph_flash_counts.pkl')
                 
-                # for run in data[int(N)].keys():
-                #     if not (np.max(data[int(N)][run]) == N):
-                #         save_flash_counts_00[N][C] += 1
-                #     if not (np.max(data[int(N - (N * 0.05))][run]) == N):
-                #         save_flash_counts_005[N][C] += 1
-                #     if not (np.max(data[int(N - (N * 0.1))][run]) == N):
-                #         save_flash_counts_01[N][C] += 1
-                #     if not (np.max(data[int(N - (N * 0.2))][run]) == N):
-                #         save_flash_counts_02[N][C] += 1
+                # check async runs
+                for run in data[int(N)].keys():
+                    if not (np.max(data[int(N)][run]) == N):
+                        save_flash_counts_00_async[N][C] += 1
+                    if not (np.max(data[int(N - (N * 0.05))][run]) == N):
+                        save_flash_counts_005_async[N][C] += 1
+                    if not (np.max(data[int(N - (N * 0.1))][run]) == N):
+                        save_flash_counts_01_async[N][C] += 1
+                    if not (np.max(data[int(N - (N * 0.2))][run]) == N):
+                        save_flash_counts_02_async[N][C] += 1
+                        
+                # gios approach
                 for run in data[int(N)].keys():
                     # if np.max(data[int(N)][run])
                     save_flash_counts_00[N][C] += np.max(data[int(N)][run]) / N
@@ -84,12 +113,21 @@ else:
                 save_flash_counts_005[N][C] = np.nan
                 save_flash_counts_01[N][C] = np.nan
                 save_flash_counts_02[N][C] = np.nan
+                save_flash_counts_00_async[N][C] = np.nan
+                save_flash_counts_005_async[N][C] = np.nan
+                save_flash_counts_01_async[N][C] = np.nan
+                save_flash_counts_02_async[N][C] = np.nan
                 print(f"File {N}/{C} not found.")
     
     heatmap_00 = np.zeros((len(Cs), len(Ns)))
     heatmap_005 = np.zeros((len(Cs), len(Ns)))
     heatmap_01 = np.zeros((len(Cs), len(Ns)))
     heatmap_02 = np.zeros((len(Cs), len(Ns)))
+    
+    heatmap_00_async = np.zeros((len(Cs), len(Ns)))
+    heatmap_005_async = np.zeros((len(Cs), len(Ns)))
+    heatmap_01_async = np.zeros((len(Cs), len(Ns)))
+    heatmap_02_async = np.zeros((len(Cs), len(Ns)))
 
     for i, C in enumerate(Cs):
         for j, N in enumerate(Ns):
@@ -97,12 +135,22 @@ else:
             heatmap_005[i, j] = save_flash_counts_005.get(N, {}).get(C, np.nan)
             heatmap_01[i, j] = save_flash_counts_01.get(N, {}).get(C, np.nan)
             heatmap_02[i, j] = save_flash_counts_02.get(N, {}).get(C, np.nan)
+            
+            heatmap_00_async[i, j] = save_flash_counts_00_async.get(N, {}).get(C, np.nan)
+            heatmap_005_async[i, j] = save_flash_counts_005_async.get(N, {}).get(C, np.nan)
+            heatmap_01_async[i, j] = save_flash_counts_01_async.get(N, {}).get(C, np.nan)
+            heatmap_02_async[i, j] = save_flash_counts_02_async.get(N, {}).get(C, np.nan)
     
     # store compressed result matrix
     np.savez_compressed(heatmap_path_00, arr=heatmap_00)
     np.savez_compressed(heatmap_path_005, arr=heatmap_005)
     np.savez_compressed(heatmap_path_01, arr=heatmap_01)
     np.savez_compressed(heatmap_path_02, arr=heatmap_02)
+    
+    np.savez_compressed(heatmap_path_00_async, arr=heatmap_00_async)
+    np.savez_compressed(heatmap_path_005_async, arr=heatmap_005_async)
+    np.savez_compressed(heatmap_path_01_async, arr=heatmap_01_async)
+    np.savez_compressed(heatmap_path_02_async, arr=heatmap_02_async)
 
 print(heatmap_00)
 
