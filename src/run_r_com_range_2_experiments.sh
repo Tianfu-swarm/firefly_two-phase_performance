f253152106acaf20105674ef5e0cf3df9696a50c
tilly <till.aust@uni-konstanz.de>
2026-04-23 04:32:36 +0200

    synch

--- src/run_r_com_range_2_experiments.sh
+++ src/run_r_com_range_2_experiments.sh
@@ -3,7 +3,7 @@ T=10000
 r_range=(1.5)  #
 Ns=(50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200)  # number of fireflies
 Cs=(10 14 18 22 26 30 34 38 42 46 50 54 58 62 66 70)  # 50 seems to be the upper limit considering T=1000
-update_noises=(0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)  # 0.0 0.05 0.1 0.2
+update_noises=(0.3 0.4 0.5 0.6 0.7 0.8 0.9)  # 0.0 0.05 0.1 0.2
 
 
 for N in "${Ns[@]}"; do
